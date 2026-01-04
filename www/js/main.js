72f201d09324fb7be249f049b500b6ff5a0172ce
Anatoly Shchurenko <fimus6777@gmail.com>
2026-01-04 19:24:03 +0300

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,8 +1,1 @@
 // made by mr.chronica
-console.log("made by mr.chronica");
-var number = 10;
-var number_2 = 8;
-console.log(number + " + " + number_2 + " = " + (number+number_2));  
-console.log(number + " - " + number_2 + " = " + (number-number_2)); 
-console.log(number + " / " + number_2 + " = " + (number/number_2)); 
-console.log(number + " * " + number_2 + " = " + (number*number_2));  
