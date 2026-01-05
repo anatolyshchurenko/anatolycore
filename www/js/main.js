704f13fc4f6a70f3a0776a27e2f0d2252af3f2bf
Anatoly Shchurenko <fimus6777@gmail.com>
2026-01-05 18:22:51 +0300

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,27 +1,13 @@
 // made by mr.chronica
-// var num = 15;
-// var stroka = "anatolycore"
+// for(var i = 1; i <= 128; i *= 2)
+// {
+//     console.log(i);
+// }
 
-// if(num == 15)
-// {
-//     console.log("num = " + num);
-//     console.log("Ok");
-// }
-// else
-// {
-//     console.log("num = " + num);
-//     console.error("Error!");
-// }
-    
-// switch(stroka)
-// {
-//     case "anatolycore": console.log("это анатоликорчик"); break;
-//     case "pippucka": console.log("это докс,сват,хана"); break;
-//     case "dox": console.log("это нельзя!"); break;
-// }
-var arr = [5,15,20,25,30,35];
-var matrix = [[5,15,20],[25,30,35]];
-console.log("массив:" + arr);
-console.log("длина массива:" + arr.length);
-console.log(matrix);
-
+var j = 0;
+while(j <= 10)
+{
+    console.log(j);
+    j++;
+}
+ 
