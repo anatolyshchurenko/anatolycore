52304edb70131b4ba7f5aeac5efb85b5b7d78e1d
Anatoly Shchurenko <fimus6777@gmail.com>
2026-01-05 17:41:17 +0300

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,1 +1,21 @@
 // made by mr.chronica
+var num = 15;
+var stroka = "anatolycore"
+
+if(num == 15)
+{
+    console.log("num = " + num);
+    console.log("Ok");
+}
+else
+{
+    console.log("num = " + num);
+    console.error("Error!");
+}
+    
+switch(stroka)
+{
+    case "anatolycore": console.log("это анатоликорчик"); break;
+    case "pippucka": console.log("это докс,сват,хана"); break;
+    case "dox": console.log("это нельзя!"); break;
+}
