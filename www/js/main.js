249599ce888ec2f0a2026128a24dc3ea7c0d8dd8
Anatoly Shchurenko <fimus6777@gmail.com>
2026-01-05 17:51:44 +0300

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,21 +1,27 @@
 // made by mr.chronica
-var num = 15;
-var stroka = "anatolycore"
+// var num = 15;
+// var stroka = "anatolycore"
 
-if(num == 15)
-{
-    console.log("num = " + num);
-    console.log("Ok");
-}
-else
-{
-    console.log("num = " + num);
-    console.error("Error!");
-}
+// if(num == 15)
+// {
+//     console.log("num = " + num);
+//     console.log("Ok");
+// }
+// else
+// {
+//     console.log("num = " + num);
+//     console.error("Error!");
+// }
     
-switch(stroka)
-{
-    case "anatolycore": console.log("это анатоликорчик"); break;
-    case "pippucka": console.log("это докс,сват,хана"); break;
-    case "dox": console.log("это нельзя!"); break;
-}
+// switch(stroka)
+// {
+//     case "anatolycore": console.log("это анатоликорчик"); break;
+//     case "pippucka": console.log("это докс,сват,хана"); break;
+//     case "dox": console.log("это нельзя!"); break;
+// }
+var arr = [5,15,20,25,30,35];
+var matrix = [[5,15,20],[25,30,35]];
+console.log("массив:" + arr);
+console.log("длина массива:" + arr.length);
+console.log(matrix);
+
