1f03a6c7ee58b7c6867bf75a01edd04f80bd8ac0
Anatoly Shchurenko <fimus6777@gmail.com>
2026-01-13 20:04:02 +0300

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,13 +1,5 @@
 // made by mr.chronica
-// for(var i = 1; i <= 128; i *= 2)
-// {
-//     console.log(i);
-// }
-
-var j = 0;
-while(j <= 10)
-{
-    console.log(j);
-    j++;
-}
- 
+function infoButton()
+    {
+        alert('имиджборд для саундклауд реперов');
+    }
