d70c3b78b70eb1d581a84bfd97bece6df98628a0
Anatoly Shchurenko <fimus6777@gmail.com>
2026-02-11 20:28:09 +0300

    11.02

--- anatolycore/src/App.js
+++ anatolycore/src/App.js
@@ -1,29 +1,23 @@
 import React, { useState } from "react";
-
+import Counter from "./components/Counter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
 function App() {
 
-  const [point, setPoint] = useState(0);
-  const [value, setValue] = useState('anatolycore')
-
-  function increment() {
-    setPoint(point +1);
-  }
-  function decrement() {
-    setPoint(point -1);
-  }
   
+  const [value, setValue] = useState('anatolycore')
   return (
     <div className="App">
-      <h1>{point}</h1>
+      
       <h2>{value}</h2>
       <input 
         type="text" 
         value={value}
         onChange={event => setValue(event.target.value)}
       />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </div>
+      <PostItem post={{title: 'anatolycore', body:'лучший сайт'}} />
+      <Counter/>
+    </div> 
   );
 }
 export default App;
