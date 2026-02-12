5d722b084104c14c9dd43d920d0ad94f844106bd
Anatoly Shchurenko <fimus6777@gmail.com>
2026-02-12 18:50:10 +0300

    Update PostItem.jsx

--- anatolycore/src/components/PostItem.jsx
+++ anatolycore/src/components/PostItem.jsx
@@ -4,11 +4,12 @@ const PostItem = (props) => {
     return(
         <div className='post'>
             <div className="post__content">
-                <strong>{props.post.title}</strong>
+                <strong>{props.post.id}.</strong>
+                <strong>{props.post.title}:</strong>
                 <div>{props.post.body}</div>
             </div>
             <div className="post_del">
-                <button>Удалить</button>
+                <button style={{borderRadius: '15px', backgroundColor: 'black', color: 'white', border: 'none'}}>Удалить</button>
             </div>
         </div>
     );
