297d6a8518fca6a89a80ef4d7e1ba28c96cfa7bb
Anatoly Shchurenko <fimus6777@gmail.com>
2026-03-04 20:16:46 +0300

    Create AuthorInfo.jsx

---
+++ anatolycore/src/components/Other/AuthorInfo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function AuthorInfo(){
+    return(
+        <div>
+            <a href="https://t.me/anatoly_shchurenko" target="_blank" rel="noreferrer">
+                <b>telegram</b>
+            </a><br></br>
+                
+            <a href="https://soundcloud.com/anatolyshchurenko" target="_blank" rel="noreferrer">
+                <b>soundcloud</b>
+            </a><br></br>
+                
+            <a href="https://discord.gg/uUfEdK9wmH" target="_blank" rel="noreferrer">
+                <b>discord</b>
+            </a><br></br>
+                
+            <a href="https://github.com/anatolyshchurenko" target="_blank" rel="noreferrer">
+                <b>github</b>
+            </a>
+        </div>
+    );
+}
