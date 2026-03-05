def1dd197d673b30e9b3d7982015a8007b7fe12e
Anatoly Shchurenko <fimus6777@gmail.com>
2026-03-05 20:05:33 +0300

    Update App.js

--- anatolycore/src/App.js
+++ anatolycore/src/App.js
@@ -4,10 +4,11 @@ import Logo from "./components/UI/Header/Logo/Logo";
 import Navigations from "./components/UI/Header/Navigations/Navigations";
 import DesInfo from "./components/UI/Header/Navigations/DesInfo";
 import AuthorInfo from "./components/Other/AuthorInfo";
+import InfoSpaceText from "./components/Other/InfoSpaceText";
+import Holder from "./components/UI/Posts/Holdersrc/Holder";
+import Createpost from "./components/UI/Posts/Create/Createpost";
 function App() {
-
-
-
+  
   return (
     <div className="App">
       {/* главный логотип */}
@@ -20,15 +21,13 @@ function App() {
       <Navigations>автор</Navigations>  
 
       {/* текст внутри блоков навигации */}
-      <DesInfo InfoSpace={
-        <div>
-          Данный сайт являет <b>полностью анонимным</b> имиджбордом 
-          нацеленным для саундклауд исполнителей
-        </div>
-      }/>
+      <DesInfo InfoSpace={<InfoSpaceText/>}/>
       <DesInfo/>
       <DesInfo/>
       <DesInfo InfoSpace={<AuthorInfo/>}/>
+
+      <Holder/>
+      <Createpost/>
     </div> 
   );
 }
