6d099af66e7f5768e8c2cf8640789d8657a48f71
Anatoly Shchurenko <fimus6777@gmail.com>
2026-03-05 20:06:02 +0300

    Create InfoSpaceText.jsx

---
+++ anatolycore/src/components/Other/InfoSpaceText.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function InfoSpaceText(){
+    return(
+        <div>
+            Данный сайт являет<br></br> 
+            <b>полностью анонимным</b><br></br>
+            имиджбордом 
+            нацеленным на<br></br> 
+            саундклауд исполнителей
+        </div>
+    );
+}
