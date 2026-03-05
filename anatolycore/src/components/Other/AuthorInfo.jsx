c5ae66429a9c91e1ee0ef751086605858f83eb3e
Anatoly Shchurenko <fimus6777@gmail.com>
2026-03-05 20:05:51 +0300

    Update AuthorInfo.jsx

--- anatolycore/src/components/Other/AuthorInfo.jsx
+++ anatolycore/src/components/Other/AuthorInfo.jsx
@@ -1,21 +1,23 @@
 import React from "react";
+import classes from "./OtherComp.module.css"
 
 export default function AuthorInfo(){
     return(
         <div>
-            <a href="https://t.me/anatoly_shchurenko" target="_blank" rel="noreferrer">
+            <p className={classes.mr_chronica}>mr. chronica</p>
+            <a className={classes.otherHref} href="https://t.me/anatoly_shchurenko" target="_blank" rel="noreferrer">
                 <b>telegram</b>
-            </a><br></br>
+            </a>
                 
-            <a href="https://soundcloud.com/anatolyshchurenko" target="_blank" rel="noreferrer">
+            <a className={classes.otherHref} href="https://soundcloud.com/anatolyshchurenko" target="_blank" rel="noreferrer">
                 <b>soundcloud</b>
-            </a><br></br>
+            </a>
                 
-            <a href="https://discord.gg/uUfEdK9wmH" target="_blank" rel="noreferrer">
+            <a className={classes.otherHref} href="https://discord.gg/uUfEdK9wmH" target="_blank" rel="noreferrer">
                 <b>discord</b>
-            </a><br></br>
+            </a>
                 
-            <a href="https://github.com/anatolyshchurenko" target="_blank" rel="noreferrer">
+            <a className={classes.otherHref} href="https://github.com/anatolyshchurenko" target="_blank" rel="noreferrer">
                 <b>github</b>
             </a>
         </div>
