7cc847c9e1ae99d8fc807b95d09f9fffbfb7289f
Anatoly Shchurenko <fimus6777@gmail.com>
2026-03-06 20:45:00 +0300

    Update Holder.jsx

--- anatolycore/src/components/UI/Posts/Holdersrc/Holder.jsx
+++ anatolycore/src/components/UI/Posts/Holdersrc/Holder.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import classes from "./Holder.module.css"
-export default function Holder() {
+export default function Holder({...props}) {
     return(
         <div>
             <textarea 
+                
                 className={classes.newPost}
                 maxlength="500" 
                 placeholder="расскажи как у тебя дела
                 （＾ω＾）"
+                {...props}
             ></textarea>
         </div>
     )
